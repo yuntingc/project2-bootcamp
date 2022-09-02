e6673cf76b8a48ff3803a28ab35763c993c208c1
yuntingc <chiayunting@gmail.com>
2022-09-02 10:59:22 +0800

    removed react strict mode

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <UserContextProvider>
-        <App />
-      </UserContextProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
+  </BrowserRouter>
 );
